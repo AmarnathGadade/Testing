96bfd217e744a9437d3888e7d8a009345c5aea85
amarnathgadade <amarnathgadade444@gmail.com>
2024-12-13 13:53:57 +0530

    case4 added

---
+++ src/main/java/Featuress/sample.feature
@@ -0,0 +1,6 @@
+Feature: Sample Feature File
+  Scenario:  Sample Scenario for login
+    Given Given the user opens the app
+    When the user enters the credentials
+    And the user clicks on login button
+    Then the user should be able to login
---
+++ src/main/java/stepdefintions/methodsteps.java
@@ -0,0 +1,22 @@
+package stepdefintions;
+
+public class methodsteps {
+    @Given("Given the user opens the app")
+    public void given_the_user_opens_the_app() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("the user enters the credentials")
+    public void the_user_enters_the_credentials() {
+
+    }
+    @When("the user clicks on login button")
+    public void the_user_clicks_on_login_button() {
+
+    }
+    @Then("the user should be able to login")
+    public void the_user_should_be_able_to_login() {
+
+    }
+
+}
